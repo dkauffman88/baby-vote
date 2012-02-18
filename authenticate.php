d8a229078902e3d94b0784c48402a1f11af96989
ed <ed@beethoven>
2012-02-18 18:21:06 -0500

    User auth example works.  Database updated

---
+++ authenticate.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+	$username = $_POST["username"]; // This is the inputted username from the form in login.html
+	$password = $_POST["password"]; // This is the inputted password from the form in login.html
+
+    include db_pass.php;
+
+	$sql="SELECT * FROM users WHERE username='$username' AND password='$password'"; 
+	$result=mysql_query($sql);
+	$result=mysql_fetch_array($result);
+	
+	echo "<h1>" . $username . "</h1>";
+	echo "<h1>" . $password . "</h1>";
+	echo $result;
+
+	// Mysql_num_row is counting table row
+	$count=1; // Check if supplied user, password, match database
+
+	if($count==1) // If there is a match..
+	{
+		$_SESSION["username"] = $username; // Creates a cookie saving the username
+		$_SESSION["loggedIn"] = true; // Creates a cookie saying the user is logged in
+		header("Location:home.php");
+	}else{ // If invalid information was entered
+		header("Location:login.html"); // Lastly, redirect back to login page
+	}
+?>
---
+++ home.php
@@ -0,0 +1,41 @@
+<?php
+	session_start();
+	if(!$_SESSION['loggedIn']) // If the user IS NOT logged in, forward them back to the login page
+	{
+		header("location:login.html");
+	}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title>My Website - Protected Content</title>
+		<link href="../../image/prophet.jpg" rel="'Shortcut" type="image/x-icon/"><br /><link rel="icon" href="../../image/prophet.jpg" type="image/x-icon">
+
+		<!-- Google Analytics -->
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-24507087-1']);
+		  _gaq.push(['_trackPageview']);
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
+	</head>
+<body>
+	<?php
+		if(!$_SESSION['loggedIn']) // If the user IS NOT logged in, forward them back to the login page
+		{
+			header("location:Login.html");
+		}else{ // If the user IS logged in, then echo the page contents:
+			$currentUser = $_SESSION['username']; // Gets the username from the cookie we created in Check.php
+			$message = '<p>Welcome, ' . ucfirst($currentUser) . '!</p>'; // This compiles hello (your username)
+			echo $message; // This echo's (actually outputs) the message
+		}
+	?>
+	<p>Good job! You got it working!!</p>
+	<a href="Logout.php">Logout</a>
+</body>
+</html>
