4134900a879fec837f318989c4189f59b06f3fe0
ed <ed@beethoven>
2012-02-18 18:57:07 -0500

    Logged in users are shown the pictures they've uploaded

--- home.php
+++ home.php
@@ -1,11 +1,3 @@
-<?php
-	session_start();
-	if(!$_SESSION['loggedIn']) // If the user IS NOT logged in, forward them back to the login page
-	{
-		header("location:login.html");
-	}
-?>
-
 ﻿<!DOCTYPE HTML>
 <html>
 	<head>
@@ -26,6 +18,8 @@
 	</head>
 <body>
 	<?php
+	
+	session_start();
 		if(!$_SESSION['loggedIn']) // If the user IS NOT logged in, forward them back to the login page
 		{
 			header("location:login.html");
@@ -33,6 +27,24 @@
 			$currentUser = $_SESSION['username']; // Gets the username from the cookie we created in Check.php
 			$message = '<p>Welcome, ' . ucfirst($currentUser) . '!</p>'; // This compiles hello (your username)
 			echo $message; // This echo's (actually outputs) the message
+			
+			include 'db_pass.php';
+			
+	        mysql_connect("localhost",$user,$password);
+            @mysql_select_db($database) or die( "Unable to select database");
+            
+            $query = "SELECT id FROM users WHERE username='$currentUser'";
+            $result = mysql_query($query);
+            $id = mysql_result($result, 0);
+            
+            $query = "SELECT path FROM babys WHERE user='$id'";
+            $result = mysql_query($query);
+            $images = mysql_fetch_array($result, MYSQL_NUM);
+            foreach ($images as $path) {
+                echo "<img src='./pics/$path'><br/>";
+            }
+            
+			
 		}
 	?>
 	<p>Good job! You got it working!!</p>
