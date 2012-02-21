513d53fd5cdb62d47a303e32df8e6f5b39b8872f
ed <ed@beethoven>
2012-02-20 19:04:38 -0500

    Fixed the login issues

--- authenticate.php
+++ authenticate.php
@@ -1,27 +1,30 @@
+﻿
 <?php
-	session_start();
+	//session_start();
 	$username = $_POST["username"]; // This is the inputted username from the form in login.html
-	$password = $_POST["password"]; // This is the inputted password from the form in login.html
-
-    include db_pass.php;
-
-	$sql="SELECT * FROM users WHERE username='$username' AND password='$password'"; 
-	$result=mysql_query($sql);
-	$result=mysql_fetch_array($result);
+	$u_password = $_POST["password"]; // This is the inputted password from the form in login.html
 	
-	echo "<h1>" . $username . "</h1>";
-	echo "<h1>" . $password . "</h1>";
-	echo $result;
+	echo "<h1>$username $u_password</h1>";
+    
+    include 'db_pass.php';
+    
+    mysql_connect("localhost",$user,$password);
+    @mysql_select_db($database) or die( "Unable to select database");
 
-	// Mysql_num_row is counting table row
-	$count=1; // Check if supplied user, password, match database
+	$query = "SELECT * FROM users WHERE username='" . $username . "' AND password='" . $u_password . "'";
+	$result = mysql_query($query);
 
-	if($count==1) // If there is a match..
+	if(mysql_num_rows($result) == 1) // If there is a match..
 	{
+	    session_start();
 		$_SESSION["username"] = $username; // Keeps this user logged in the the SESSION var
 		$_SESSION["loggedIn"] = true; // Lets us know somebody is logged in
 		header("Location:home.php");
-	}else{ // If invalid information was entered
+		//echo "<h1>Logged in!</h1>";
+	}
+	
+	else{ // If invalid information was entered
 		header("Location:login.html"); // Lastly, redirect back to login page
 	}
 ?>
+
